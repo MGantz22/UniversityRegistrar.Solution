1c308ba334ca587379b3ae9385b554056d22f52e
Mitchell Gantz <mgantz22@icloud.com>
2023-03-13 11:57:21 -0700

    updating students and courses controllers Co-authored-by: John <john.lenz27@gmail.com>

--- Registrar/Controllers/CoursesController.cs
+++ Registrar/Controllers/CoursesController.cs
@@ -8,6 +8,80 @@ namespace Registrar.Controllers
 {
   public class CoursesController : Controller
   {
-    
+    private readonly RegistrarContext _db;
+    public CoursesController(RegistrarContext db)
+    {
+      _db = db;
+    }
+
+  public ActionResult Index()
+  {
+    ViewBag.PageTitle = "View All Courses";
+    List<Course> CourseList = _db.Courses.Include(course => course.JoinEntities)
+                                          .ThenInclude(join => join.Student)
+                                          .OrderByDescending(course => courseJoinEntities.Count)
+                                          .ToList();
+      return View(CourseList);
+    }
+
+    public ActionResult Details(int id)
+    {
+      Course thisCourse =_db.Course
+                              .Include(course => course.JoinEntities)
+                              .ThenInclude(join => join.Student)
+                              .FirstOrDefault(course => course.CourseId == id);
+      ViewBag.PageTitle = $"{thisCourse.Number} Details";
+    return View(thisCourse);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "Add Course";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResut Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddStudent(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      ViewBag.PageTitle = "Add Student";
+      return View(thisCourse);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Course course)
+    {
+      _db.Courses.Update(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      return View(thisCourse);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Course thisCourse = _db.Courses.FirstOrDefault(courses => courses.CourseId == id);
+      _db.Courses.Remove(thisCourse);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- Registrar/Controllers/StudentsController.cs
+++ Registrar/Controllers/StudentsController.cs
@@ -11,14 +11,14 @@ namespace Registrar.Controllers
     {
         private readonly RegistrarContext _db;
 
-        public StudentsController(Registrar db)
+        public StudentsController(RegistrarContext db)
         {
             _db = db;
         }
 
         public ActionResult Index()
         {
-            List<Student> model = _db.Items
+            List<Student> model = _db.Students
                                     .Include(student => student.Course)
                                     .ToList();
             return View(model);
@@ -42,8 +42,44 @@ namespace Registrar.Controllers
             return RedirectToAction("Index");
         }
 
-  
+        public ActionResult Details(int id)
+        {
+            Student thisStudent = _db.Students
+                .Include(student => student.Course)
+                .Include(student => student.JoinEntities)
+                .ThenInclude(join => join.Tag)
+                .FirstOrDefault(student => student.StudentId == id);
+            return View(thisStudent);
+        }
 
+        public ActionResult Edit(int id)
+        {
+            Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+            ViewBag.CourseId = new SelectList(_db.Courses, "CourseId", "CourseName");
+            return View(thisStudent);
+        }
 
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            _db.Students.Update(student);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+            return View(thisStudent);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student thisStudent = _db.Students.FirstOrDefault(student => student.StudentsId == id);
+            _db.Students.Remove(thisStudent);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
